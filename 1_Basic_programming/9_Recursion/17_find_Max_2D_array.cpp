2e64faae0f297991a94ea0c965ef17beb14d0457
Jehan Hasan <jhasan0122@gmail.com>
2024-05-23 07:28:31 +0100

    half

---
+++ 1_Basic_programming/9_Recursion/17_find_Max_2D_array.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+
+using namespace std;
+
+int findMax2DArr(int n,int m,int matrix[][m]){
+
+}
+
+int main(){
+    int matrix[3][4]={
+            {19,92,38,4},
+            {5,96,73,88},
+            {34,46,75,91}
+    };
+}
